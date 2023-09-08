677631870b906cb3e9c35a5041eb365cc0b988d0
Mfonisocletus <mfonisocletus124@gmail.com>
2023-09-08 08:13:51 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,7 @@
   *a piece of art, " followed new line.
   *Return: Always 0.
  */
-
-int main(void)
-	   
-
+int main(void)	   
 {
        printf("with proper grammar, but the outcome is a piece of art,\n");
        return (0);
