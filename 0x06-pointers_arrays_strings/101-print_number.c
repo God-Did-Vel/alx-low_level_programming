0d34e46587acd8dc950f246b8e537a60066fd996
Mfonisocletus <mfonisocletus124@gmail.com>
2023-09-21 13:00:57 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,7 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int n1;
 	if (n < 0)
 	{
 		_putchar('-');
@@ -16,7 +16,8 @@ void print_number(int n)
 	}
 
 	if ((num / 10) > 0)
+	{
 		print_number(num / 10);
-
+	}
 	_putchar((num % 10) + '0');
 }
