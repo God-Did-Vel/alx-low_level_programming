5fa784d6429c3106d787d0d1301788518a73e99c
Mfonisocletus <mfonisocletus124@gmail.com>
2023-10-08 10:05:44 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,7 +7,6 @@
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 
 int _putchar(char c);
-void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int len(char *str);
@@ -19,8 +18,5 @@ char *create_xarray(int size);
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
 
 #endif /*MAIN_H*/
