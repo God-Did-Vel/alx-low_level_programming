6bd5f4c3ca1646042a73a8f46d0e845b7a7b2d6d
Mfonisocletus <mfonisocletus124@gmail.com>
2023-12-11 11:15:55 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * sub - subtract two integers
+ * @a: first unt
+ * @b: second int
+ * Return: subtracted final result
+ */
+int subtract(int a, int b)
+{
+	return(a - b);
+}
