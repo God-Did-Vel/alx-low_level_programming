ab4b89b7bfe8e064cad05594f62695672c58ddb7
Mfonisocletus <mfonisocletus124@gmail.com>
2023-12-11 18:07:32 -0800

    all.c

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: first unt
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+        return (a + b);
+}
+/**
+ * sub - subtract two integers
+ * @a: first unt
+ * @b: second int
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+        return(a - b);
+}
+/**
+ * mul - multiple two integers
+ * @a: first unt
+ * @b: second int
+ * Return: multiplication 
+ */
+int mul(int a, int b)
+{
+        return(a * b);
+}
+/**
+ * div - divide two integers
+ * @a: first unt
+ * @b: second int
+ * Return: division 
+ */
+int div(int a, int b)
+{
+        return(a / b);
+}
+/**
+ * mod- mod two integers
+ * @a: first unt
+ * @b: second int
+ * Return: final result
+ */
+int mod(int a, int b)
+{
+        return(a % b);
+}
+
